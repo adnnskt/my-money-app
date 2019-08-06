d46181b8fc67eae4965e6eaebca380853f483439
adnnskt <adnnskt@gmail.com>
2019-08-06 20:47:46 -0300

    auth user

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
+import AuthOrApp from './authOrApp'
 import Dashboard from '../dashboard/dashboard'
-//import Dashboard from '../dashboard2/dashboard2'
 import BillingCycle from '../billingCycle/billingCycle'
 
+
+export default props => (
+    <Router history={hashHistory}>
+        <Route path='/' component={AuthOrApp}>
+            <IndexRoute component={Dashboard} />
+            <Route path='billingCycles' component={BillingCycle} />
+        </Route>
+        <Redirect from='*' to='/' />
+    </Router>
+)
+
+
+
+/*
 export default props => (
     <Router history={hashHistory}>
         <Route path='/' component={Dashboard} />
@@ -12,3 +26,4 @@ export default props => (
         <Redirect from='*' to='/' />    
     </Router>
 )
+*/
